e6cf9100b55759d2c4b994a7c477bec6ecc2d5f4
juraspb <juraspb@icloud.com>
2022-06-23 14:51:36 +0300

    230622 backup

--- source/typhoon.cpp
+++ source/typhoon.cpp
@@ -29,8 +29,8 @@ void signalHandler (int s){
 
 int camera(string URL)
 {
-  namedWindow(URL, WINDOW_AUTOSIZE);
-  //resizeWindow(URL, 640, 480);
+  namedWindow(URL, WINDOW_NORMAL);
+  resizeWindow(URL, 640, 480);
   //moveWindow(URL, 0, 0);
 
   Mat frame,gray;
@@ -39,8 +39,9 @@ int camera(string URL)
   while(!done) {
     if (cap.read(frame)) {
       //cout << frame.size()<<endl;
-      cvtColor(frame, gray, COLOR_BGR2GRAY);
-      imshow(URL, gray);
+      //cvtColor(frame, gray, COLOR_BGR2GRAY);
+      //imshow(URL, gray);
+      imshow(URL, frame);
     }	
     else {
       return -1;
@@ -63,59 +64,20 @@ int main()
   // When CTRL+C pressed, signalHandler will be called to interrupt the programs execution
   signal(SIGINT, signalHandler);
 
-	//string cam1_url = "rtsp://root:root@192.168.1.99:554/av0_1";
-  //string cam1_url = "rtsp://admin:pP@697469@192.168.1.102:554/Stream/Channel/101" ;
-  //string cam2_url = "rtsp://user:9999@192.168.1.167:8557/PSIA/Streaming/channels/2?videoCodecType=H.264";
-  //string filename="/home/yuriy/Videos/ul1.rec";
-  
-
-  //string URL = "rtsp://root:root@192.168.1.12/video_1";
-  //string URL = "rtsp://192.168.11.167:8555/PSIA/Streaming/channels/0?videoCodecType=MJPEG";
-  //string URL = "rtsp://192.168.11.167:8553/PSIA/Streaming/channels/1?videoCodecType=MPEG4";
-  string URL = "rtsp://admin:pP@697469@192.168.1.102:554/Stream/Channel/101";
+  //string URL = "/home/yuriy/Videos/ul1.rec";                                              //from files
+  //string URL = "rtsp://admin:pP@697469@192.168.1.102:554/Stream/Channel/101";             //hikvision
+	//string URL = "rtsp://root:root@192.168.1.99:554/av0_1";                                 //beward
+  //string URL = "rtsp://root:root@192.168.1.12/video_1";                                   //ves-257
+  string URL = "rtsp://192.168.11.167:8553/PSIA/Streaming/channels/1?videoCodecType=MPEG4"; // ves-556
 
   thread cam2(camera,URL);
 
-  //namedWindow(URL, WINDOW_AUTOSIZE);
-  //moveWindow(URL, 0, 0);
-  //namedWindow(URL2, WINDOW_AUTOSIZE);
-  //moveWindow(URL2, 800, 0);
-  //Mat frame,gray;
-  //VideoCapture cap;
-  //cap.open(URL);
-  //VideoCapture cap2;
-  //cap2.open(URL2);
   int i = 0;
   while(!done)
   {
     this_thread::sleep_for(milliseconds(500));
     cout << i << endl;
     i++;
-
-
-/*
-    if (cap.read(frame)) 
-    {
-      //cout << frame.size()<<endl;
-      //cvtColor(frame, gray, COLOR_BGR2GRAY);
-      imshow(URL, frame);
-    }	
-    else
-    {
-      return -1;
-    }
-
-    if (cap2.read(frame)) 
-    {
-      //cout << frame.size()<<endl;
-      cvtColor(frame, gray, COLOR_BGR2GRAY);
-      imshow(URL2, gray);
-    }	
-    else
-    {
-      return -1;
-    }
-*/    
     if (key=='q') break;
   }  
   // but call there
